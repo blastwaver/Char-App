52fdfd51bb092b0fa6adb2c20a328feff5f1a112
Kyeongho Na <blastwaver.k@gmail.com>
2017-12-06 16:22:43 -0500

    chat-app login part

---
+++ src/Factories.js
@@ -0,0 +1,40 @@
+const uuidv4 = require('uuid/v4');
+
+const createUser = ({name = ""} ={}) => (
+    {
+        id:uuidv4(),
+        name
+    }
+);
+
+// const createUser = function(name) {
+//     return {id:uuidv4(), name:name};
+// }
+const createMessage = ({message = "", sender =""}={ }) =>(
+    {
+        id:uuidv4(),
+        time: new getTime(Date.now()),
+        message,
+        sender
+    }
+);  
+
+const createChat = ({messages =[], name = "Community", users = []} = {}) =>(
+    {
+        id:uuidv4(),
+        name,
+        messages,
+        users,
+        typingUsers:[]
+    }
+)
+
+const getTime = (date) => {
+    return `${date.getHour()}:${("0"+date.getMinutes()).slice(-2)}`
+}
+
+module.export = {
+    createChat,
+    createUser,
+    createMessage
+};
---
+++ src/server/SocketManager.js
@@ -0,0 +1,51 @@
+const io = require('./index.js').io;
+
+const { VERIFY_USER, USER_CONNECTED, LOGOUT } = require('../Events');
+
+
+const { createUser, createMessage, createChat } = require('../Factories');
+
+const connectedUsers = { };
+
+module.exports = function(socket) {
+    
+    console.log("Socket Id:" + socket.id);
+
+    //verify username
+    socket.on(VERIFY_USER, function(nickname, callback){
+
+    //    console.log( isUser(connectedUsers, nickname));
+
+        if(!isUser(connectedUsers, nickname)){
+
+          callback({isUser:false, user: createUser({name:nickname})});
+
+        }else{
+         
+          callback({isUser:true})
+    
+        }
+      })
+    //user connected user name
+
+    //user disconneceted
+
+    //users logouts
+}
+
+function addUser(userList, username){
+    let newList = Object.assign({}, userList);
+    newList[user.name] = username;
+    return newList;
+}
+
+function removeUser(userList, username){
+    let newList = Object.assign({}, userList);
+    delete newList[username];
+    return newList;
+}
+
+function isUser(userList, username){
+    //if username is in the list, returns true, otherwise return false
+    return username in userList;
+}
